fbc0e2d5d944af514f0977660cf7a6205197697f
Mahmud Hasan <mahmudhassan11752@gmail.com>
2023-08-09 19:23:23 +0600

    landing page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
+import NavBar from "../Pages/Shared/NavBar/NavBar";
+import SideMenu from "../Pages/SideMenu/SideMenu";
+
 const Main = () => {
   return (
     <div>
-      <h2>pixel editor </h2>
-      <p>we will work here</p>
-      <p>end game</p>
-      <p>Today</p>
-      <p>acknowledge from Tahmid Tuhin </p>
+      <NavBar></NavBar>
+      <div className="bg-black">
+      <SideMenu></SideMenu>
+      </div>
     </div>
   );
 };
---
+++ src/Pages/SideMenu/SideMenu.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import {
+  FaDesktop,
+  FaDropbox,
+  FaGoogleDrive,
+  FaHome,
+} from "react-icons/fa";
+import { GrOnedrive } from "react-icons/Gr";
+
+const SideMenu = () => {
+  return (
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center mt-5">
+        {/* Page content here */}
+        <Outlet></Outlet>
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-primary drawer-button lg:hidden"
+        >
+          Open drawer
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-60 h-full bg-gray-200 text-base-content">
+          {/* Sidebar content here */}
+          <li className="text-center">
+            <Link to="/home" className="sidebar-link">
+              <FaHome className="sidebar-icon" /> Home
+            </Link>
+          </li>
+          <li className="text-center">
+            <Link to="/device" className="sidebar-link">
+              <FaDesktop className="sidebar-icon" /> This Device
+            </Link>
+          </li>
+          <li className="text-center">
+            <Link to="/dropbox" className="sidebar-link">
+              <FaDropbox className="sidebar-icon" /> Dropbox
+            </Link>
+          </li>
+          <li className="text-center">
+            <Link to="/oneDrive" className="sidebar-link">
+              <GrOnedrive className="sidebar-icon" /> OneDrive
+            </Link>
+          </li>
+          <li className="text-center">
+            <Link to="/googleDrive" className="sidebar-link">
+              <FaGoogleDrive className="sidebar-icon" /> Google Drive
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default SideMenu;
