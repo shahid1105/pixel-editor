b41f076c6606cc03f1494360ace8db157aa47c95
shahid1105 <shahidhasan1105@gmail.com>
2023-08-18 19:25:00 +0600

    some essue fixed

--- src/main.jsx
+++ src/main.jsx
@@ -4,21 +4,16 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import router from "./Routes/Routes.jsx";
-<<<<<<< HEAD
 import store from "./Redux/Store";
+import AuthProvider from "./Pages/Providers/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* Wrap the App component with the Provider */}
     <Provider store={store}>
+      <AuthProvider> 
       <RouterProvider router={router} />
+      </AuthProvider>
     </Provider>
-=======
-import AuthProvider from "./Pages/Providers/AuthProvider";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-   <AuthProvider> <RouterProvider router={router} /></AuthProvider>
->>>>>>> d80b25f2e4a9bb5f7225712206f6e9ea7237c50d
   </React.StrictMode>
 );
