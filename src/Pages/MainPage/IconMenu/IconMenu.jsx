a88355244dc5212f4208c5f0bc568fa261381b0d
shahid1105 <shahidhasan1105@gmail.com>
2023-08-25 02:14:20 +0600

    some changes

--- src/Pages/MainPage/IconMenu/IconMenu.jsx
+++ src/Pages/MainPage/IconMenu/IconMenu.jsx
@@ -7,10 +7,9 @@ import { FaPaintBrush } from "react-icons/fa";
 import { BsChevronDoubleLeft, BsChevronDoubleRight } from "react-icons/bs";
 import { MdLensBlur } from "react-icons/md";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { LuImagePlus } from "react-icons/Lu";
-import { ReactCrop } from "react-image-crop";
+import SideMenu from "../../SideMenu/SideMenu";
 import ImageEditor from "../../ImageEditor/ImageEditor";
+import { useSelector } from "../../../Redux/Store";
 
 const IconMenu = () => {
   const selectedImage = useSelector((state) => state.selectedImage);
@@ -148,6 +147,29 @@ const IconMenu = () => {
               onChangeComplete={handleOnchange}
             />
           </div>
+        </div>
+      </div>
+      <div className="icone h-[600px] grid grid-cols-1 md:grid-cols-12 gap-0 md:gap-1">
+        <div className={`bg-slate-600 text-white`}>
+          <button onClick={toggleColumns} className="text-sm ml-3">
+            {columns === 1 ? <BsChevronDoubleLeft /> : <BsChevronDoubleRight />}
+          </button>
+          <div
+            className={`p-4 grid grid-cols-10 md:grid-cols-${columns} gap-2 bg-slate-700 text-white`}>
+            <Icon></Icon>
+          </div>
+        </div>
+        <div className="col-span-8 bg-purple-400">
+          <ImageEditor></ImageEditor>
+        </div>
+        <div
+          style={{ backgroundColor: currentColor }}
+          className="p-5 col-span-3 ">
+          <SketchPicker
+            className="mx-auto"
+            color={currentColor}
+            onChangeComplete={handleOnchange}
+          />
         </div>
       </div>
     </>
